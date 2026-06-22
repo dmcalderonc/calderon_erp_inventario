063794947eccb64f4bc8f962c78198e9ce354c69
dmcalderonc <dmcalderonc97@gmail.com>
2026-06-22 11:51:45 -0500

    feat: added swager

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mi doc')
+    .setDescription('Descripción de mi doc')
+    .setVersion('1.0')
+    .addTag('usuarios')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  
+  console.log(`Aplicación corriendo en: http://localhost:${port}`);
+  console.log(`Documentación de Swagger disponible en: http://localhost:${port}/doc`);
 }
 bootstrap();
+
