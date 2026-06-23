e76aa7da71e1b2707f9db4fb2c898abcaed2a45f
dmcalderonc <dmcalderonc97@gmail.com>
2026-06-23 09:00:15 -0500

    prueba swagger 3

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   });
   
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 3001;
   await app.listen(port);
 
   await app.listen(port);
